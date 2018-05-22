27963eb47448674c8318ccf8a4ece687e416374d
skysmile3 <isaaclee1101@hanmail.net>
2018-05-22 17:58:52 +0900

    add Interface

---
+++ Pineapple_Android/app/src/main/java/org/androidtown/pineapple_android/Interface/LocationInterface.java
@@ -0,0 +1,9 @@
+package org.androidtown.pineapple_android.Interface;
+
+/**
+ * Created by MSI on 2018-05-22.
+ */
+
+public interface LocationInterface {
+    void locationChangedCallBack(double lon, double lat);
+}
